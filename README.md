1f99990bad6352d76ba56892ee7618ba9149b35a
YDKD <1606354057@qq.com>
2023-05-16 17:58:42 +0800

    feat(engineering): add commit lint and prettier lint

---
+++ README.md
@@ -0,0 +1,5 @@
+foss-cli - A command line tool for FOSS projects
+
+---
+
+I don‘t know what I‘m doing. But I‘m doing it anyway. Maybe it will work for my company or my personal project.
---
+++ commitlint.config.js
@@ -0,0 +1,46 @@
+/*
+ * @Version: 1.0
+ * @Autor: YDKD
+ * @Date: 2023-05-16 17:47:18
+ * @LastEditors: YDKD
+ * @LastEditTime: 2023-05-16 17:47:38
+ */
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'release',
+        'types'
+      ]
+    ]
+  }
+}
